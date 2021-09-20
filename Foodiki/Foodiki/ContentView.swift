2b32f61a92678ce82eb09649160a4a6a3e53a2db
Darien <r880514@gmail.com>
2021-09-20 15:41:58 +0800

    init

---
+++ Foodiki/Foodiki/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Foodiki
+//
+//  Created by Akira Akimoto on 2021/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Foodiki/Foodiki/FoodikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodikiApp.swift
+//  Foodiki
+//
+//  Created by Akira Akimoto on 2021/9/20.
+//
+
+import SwiftUI
+
+@main
+struct FoodikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
